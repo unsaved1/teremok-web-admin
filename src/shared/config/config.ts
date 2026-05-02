983c085c5679dc0c05b1cfeeab0129dcccfa8f92
unsaved1 <eelwrld@gmail.com>
2026-05-02 19:05:49 +0300

    update vite

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -3,5 +3,6 @@ import {appConfig} from '@root/app.config';
 export const config = Object.freeze({
     apiDomain: import.meta.env.VITE_API_DOMAIN,
     wsDomain: import.meta.env.VITE_API_WS_DOMAIN,
+    appProdPort: import.meta.env.VITE_APP_PROD_PORT,
     app: appConfig,
 });
