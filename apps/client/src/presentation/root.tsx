e4686e5ae1e64256a8f74e8f09b32991a18db8fa
unsaved1 <eelwrld@gmail.com>
2026-05-17 14:28:46 +0300

    add favicon

--- apps/client/src/presentation/root.tsx
+++ apps/client/src/presentation/root.tsx
@@ -44,6 +44,24 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Navbar />
